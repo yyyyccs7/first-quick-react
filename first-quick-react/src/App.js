05bc8d37e69b6e0f800dbbe8c26845e7171cef12
yyyyccs7 <531036908@qq.com>
2020-04-12 15:49:32 -0500

    slice 4

--- first-quick-react/src/App.js
+++ first-quick-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -30,7 +31,7 @@ const schedule = {
 };
 
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]' }</Title>
 );
 
 const CourseList = ({ courses }) => (
@@ -55,11 +56,27 @@ const Course = ({ course }) => (
   </Button>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title = {schedule.title}/>
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 export default App;
