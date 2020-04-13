ffa79e91708171b791b3a27232d27128c201106c
yyyyccs7 <531036908@qq.com>
2020-04-12 20:56:23 -0500

    slice 7

---
+++ first-quick-react/build/precache-manifest.0b8a079c5a08471d4ca1e61ee1d838d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7944194a80b48368442ec5cf77ea6b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a50f8e9f24583f52ac6d",
+    "url": "/static/css/2.f4da3fac.chunk.css"
+  },
+  {
+    "revision": "621beb2823174cce8567",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a50f8e9f24583f52ac6d",
+    "url": "/static/js/2.98b03d1e.chunk.js"
+  },
+  {
+    "revision": "b5145b75ee59bd4790b7b330d16ae913",
+    "url": "/static/js/2.98b03d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621beb2823174cce8567",
+    "url": "/static/js/main.ff980c24.chunk.js"
+  },
+  {
+    "revision": "27c09c0fe4828496ab34",
+    "url": "/static/js/runtime-main.cd553a99.js"
+  }
+]);
